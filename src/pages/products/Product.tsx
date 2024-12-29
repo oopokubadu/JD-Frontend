913b71c91a577670db1fd7986b1bdf305710cbfe
lntori <lntori99@gmail.com>
2024-12-29 16:40:23 +0000

    added new features

--- src/pages/products/Product.tsx
+++ src/pages/products/Product.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ProductHero from "./components/ProductHero";
+import AllProduct from "./components/AllProduct";
+import CallToAction from "../home/components/CallToAction";
 
 const Product = () => {
   return (
     <div>
       <ProductHero />
+      <AllProduct/>
+      <CallToAction/>
     </div>
   );
 };
--- src/pages/products/components/ProductHero.tsx
+++ src/pages/products/components/ProductHero.tsx
@@ -11,15 +11,15 @@ const ProductHero = () => {
           </h2>
         </div>
         <div className="hidden lg:block relative">
-          <span className="absolute top-[5rem] left-[13rem] text-nowrap rotate-[25deg] text-xs font-medium text-[#3C072E] bg-[#E9D7D5] rounded-full px-2.5 py-2">
+          <span className="absolute bottom-[5rem] left-[8rem] text-nowrap rotate-[-30deg] text-xs font-medium text-[#3C072E] bg-[#E9D7D5] rounded-full px-2.5 py-2">
             NO SUGAR ADDED
           </span>
 
-          <span className="absolute top-[1.5rem] right-[8rem] text-nowrap rotate-[-30deg] text-xs font-medium text-[#3C072E] bg-[#EDDCFF]  rounded-full px-2.5 py-1">
+          <span className="absolute bottom-[20rem] left-[20rem] text-nowrap rotate-[30deg] text-xs font-medium text-[#3C072E] bg-[#EDDCFF]  rounded-full px-2.5 py-1">
             ORGANIC
           </span>
 
-          <span className="absolute bottom-[7rem] right-[12rem] text-nowrap rotate-[-15deg] text-xs font-medium text-[#3C072E] bg-[#FDD174] rounded-full px-2.5 py-1">
+          <span className="absolute bottom-[16rem] right-[20rem] text-nowrap rotate-[-15deg] text-xs font-medium text-[#3C072E] bg-[#FDD174] rounded-full px-2.5 py-1">
             {" "}
             PURE FRUIT
           </span>
